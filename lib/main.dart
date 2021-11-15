b03eba65c2e61da6e98de387ad5b43ad8f79a29b
Felipe Sales <soufeliposales@gmail.com>
2021-11-15 19:40:01 -0300

    learning about page and named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'pages/home_page.dart';
+import 'pages/page_01.dart';
+import 'pages/page_02.dart';
+import 'pages/page_03.dart';
+import 'pages/page_04.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,7 +20,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      //home: const HomePage(),
+      routes: {
+        '/': (_) => const HomePage(),
+        '/page01': (_) => const Page01(),
+        Page02.routeName: (_) => const Page02(),
+        '/page03': (_) => const Page03(),
+        '/page04': (_) => const Page04(),
+      },
     );
   }
 }
---
+++ lib/pages/page_03.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import 'page_04.dart';
+
+class Page03 extends StatelessWidget {
+  const Page03({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page | 03'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+
+        children: [
+          ElevatedButton(
+            child: const Text('Page | 04 -> page'),
+            onPressed: () {
+              // Navigator.of(context).pushReplacement(
+              //   MaterialPageRoute(
+              //     settings: const RouteSettings(name: 'page04'),
+              //     builder: (context) => const Page04(),
+              //   ),
+              // );
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  settings: const RouteSettings(name: 'page04'),
+                  builder: (context) => const Page04(),
+                ),
+              );
+            },
+          ),
+          ElevatedButton(
+            child: const Text('Page | 03 -> pop'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          ElevatedButton(
+            child: const Text('Page | 04 -> named'),
+            onPressed: () {
+              Navigator.of(context).pushNamed('/page04');
+
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
